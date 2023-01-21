301f2be817a546b9529cd63e76e139305454ccb7
hunain haider <hunainhaider87@gamil.com>
2023-01-21 09:51:02 +0500

    create this changes

---
+++ db/migrate/20230121041614_create_studio.rb
@@ -0,0 +1,9 @@
+class CreateStudio < ActiveRecord::Migration[6.1]
+  def change
+    create_table :studios do |t|
+      t.string :name
+      t.string :email
+      t.timestamps
+    end
+  end
+end
